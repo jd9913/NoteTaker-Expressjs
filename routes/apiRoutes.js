eaa0b4c3826a7d9b308d6c85c867d71eec85d694
Jennifer Schlierman <jd9913@gmail.com>
2020-10-11 17:25:59 -0700

    complete refactor

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,34 +1,26 @@
 const router = require('express').Router();
 
-const saveNotes = require('../lib/notes');
-
+const createNote = require('../lib/notes');
+const deleteNote = require('../lib/notes');
 
 
 //GET request
 
 router.get('/notes', function (req, res) {
-    res.send(    
-    saveNotes
-        .read())
-      
 
+    createNote();
 });
 
 //POST request
 
 router.post('/notes', (req, res) => {
-    saveNotes
-        .createNote(req.body)
-    
-     res.send('working')
+    createNote(req.body);
+
+    res.send('working')
 });
 
 router.delete('/notes/:id', function (req, res) {
-    saveNotes
-        .deleteNotes(req.params.id)
-        .then(() => res.json({ ok: true }))
-        .catch(err => res.status(500).json(err));
-
+    deleteNote(req.params.id)
 
 });
 
